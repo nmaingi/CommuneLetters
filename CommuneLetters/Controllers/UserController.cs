dbc3ae77b66c9571d0b12ec8fabef830401772f1
Ngina Maingi <ngina_maingi@yahoo.com>
2021-03-14 13:21:07 -0500

    Fixed first draft Issues, all pages loading, Still to add Javascript, Website information and style.

---
+++ CommuneLetters/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommuneLetters.Controllers
+{
+    public class UserController : Controller
+    {
+        //add feature to create new clients//
+        [HttpGet]
+        public IActionResult PersonInfo() => View();
+
+        [HttpGet]
+        public IActionResult Payment() => View();
+
+        [HttpGet]
+        public IActionResult FAQs() => View();
+    }
+}
